0293d401ff4564f6d1031d619c996e1affe3a24c
Leandro Muto <leandro.muto@gmail.com>
2021-05-25 09:11:22 +0100

    initial commit

---
+++ connection.py
@@ -0,0 +1,11 @@
+import pymongo
+from pymongo import MongoClient
+
+# connect to cluster
+client = MongoClient("mongodb+srv://admin:BT68FdbkPxfbALCm@users.ix3mr.mongodb.net/coach?retryWrites=true&w=majority")
+# select the database
+db = client.olheiro
+# select the collection from database
+collection = db.events
+
+collection.insert_one({"_id": 0, "name": "test"})
