c10f7ce84ed45140672e8c4d610883f78b0ae2b6
Leandro Muto <leandro.muto@gmail.com>
2021-06-12 01:20:07 +0100

    feature: retriving running info from all states

--- running/spiders/corridasbr_spider.py
+++ running/spiders/corridasbr_spider.py
@@ -2,6 +2,7 @@ import scrapy
 
 class CorridasBRSpider(scrapy.Spider):
 	name = "corridasbr"
+	base_url = "http://www.corridasbr.com.br"
 
 	def start_requests(self):
 		states = [
@@ -12,10 +13,30 @@ class CorridasBRSpider(scrapy.Spider):
 
 		for state in states:
 			yield scrapy.Request(
-				url=f'http://www.corridasbr.com.br/{state}/calendario.asp',
-				callback=self.parse
+				url=f'{self.base_url}/{state}/calendario.asp',
+				callback=self.parse_states,
+				cb_kwargs=dict(state=state)
 			)
 
-	def parse(self, response):
-		print(response)
+	def parse_states(self, response, state):
+		next_pages = response.css('td:nth-child(3) span.tipo3 a::attr(href)').getall()
 
+		if next_pages is not None:
+			for page in next_pages:
+				yield scrapy.Request(
+					url=f"{self.base_url}/{state}/{page}",
+					callback=self.parse_state_running
+				)
+
+	def parse_state_running(self, response):
+		table_selector = "(//table)[9]"
+
+		for info in response.xpath(table_selector):
+			yield {
+				"name": info.css('tr span strong::text').get(),
+				"date": info.css('tr:nth-child(2) td:nth-child(2) span::text').get(),
+				"city": info.css('tr:nth-child(3) td:nth-child(2) span::text').get(),
+				"distances": info.css('tr:nth-child(4) td:nth-child(2) span::text').get(),
+				"organizer": info.css('tr:nth-child(6) td:nth-child(2) span::text').get(),
+				"website": info.css('tr:nth-child(7) td:nth-child(2) a::attr(href)').get()
+			}
