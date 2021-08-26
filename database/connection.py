ad9f8b20248787cca41234b11153735511cbfc31
Leandro Muto <leandro.muto@gmail.com>
2021-08-26 21:46:11 +0100

    add database connection

---
+++ database/connection.py
@@ -0,0 +1,17 @@
+import os
+from pymongo import MongoClient
+from dotenv import load_dotenv
+
+def get_database():
+    config = load_dotenv()
+
+    client = MongoClient(os.environ.get("MONGO_URI")) # connect to cluster
+    database = client.scout # select the database
+
+    return database
+
+def get_collection(name):
+    return get_database().get_collection(name)
+
+if __name__ == "__main__":
+    db = get_database()
