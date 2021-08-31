d03335b3e7bd3634f61ba5a5a19b52b092d38ed7
Leandro Muto <leandro.muto@gmail.com>
2021-08-31 23:01:25 +0100

    chore: Add new pipeline to treat race data

--- running/pipelines.py
+++ running/pipelines.py
@@ -1,6 +1,21 @@
+import re
 import logging
 # from scrapy.exceptions import DropItem
 from database.connection import get_database, get_collection
+
+class RacePipeline:
+    def __init__(self):
+        pass
+
+    def process_item(self, item, spider):
+        spider_name = spider.name
+        if spider_name == 'corridasbr':
+            item["modality"] = "running"
+
+        distances = re.findall(r"([0-9,]+)", str(item["distances"]))
+        item["distances"] = distances
+
+        return item
 
 class MongoDBPipeline:
     def __init__(self):
--- running/settings.py
+++ running/settings.py
@@ -63,6 +63,7 @@ ROBOTSTXT_OBEY = False
 # Configure item pipelines
 # See https://docs.scrapy.org/en/latest/topics/item-pipeline.html
 ITEM_PIPELINES = {
+   'running.pipelines.RacePipeline': 1,
    'running.pipelines.MongoDBPipeline': 300,
 }
 
